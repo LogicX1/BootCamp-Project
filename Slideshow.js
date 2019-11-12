0cbe0e8da51af4ca87cdad333d654a165aa1fd39
Najwan <najone93@gmail.com>
2019-11-13 01:12:37 +0200

    Changed whole method of the slideshow
    on my first attempt i created some bad code that eventually i didnt know what to do with it so re did the whole thing
    ** problem the event lisenter isnt working

--- Slideshow.js
+++ Slideshow.js
@@ -1,18 +1,10 @@
-var slideIndex = [1,1];
-var slideId = ["mySlides1"]
-showSlides(1, 0);
+/*
+document.getElementById("Main").addEventListener("keydown",checkKey,false);
 
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+function checkKey(key){
+  if(key.keycode === '39')
+    nextImage();
+  if(key.keycode === '37')
+    prevImage();
 }
-
-function showSlides(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex[no]-1].style.display = "block";
-}
+*/
